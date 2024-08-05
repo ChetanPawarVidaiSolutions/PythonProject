04a978390df31de9c0059e6ce15a313fb08ca8d2
chetan <chetan.pawar@vidaisolutions.com>
2024-08-05 16:20:59 +0530

    add project

---
+++ tasks/models.py
@@ -0,0 +1,27 @@
+# tasks/models.py
+from django.db import models
+from django.utils import timezone
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('To Do', 'To Do'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed')
+    )
+
+    PRIORITY_CHOICES = (
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High')
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    due_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
